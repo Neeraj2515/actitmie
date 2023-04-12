30100470fd16fb31efc71dbdb83f79c2e626dceb
Neeraj <Neeraj@HP>
2023-04-12 19:21:18 +0530

    neeraj

--- Automation/src/com/actitime/generics/Baseclass.java
+++ Automation/src/com/actitime/generics/Baseclass.java
@@ -1,25 +1,40 @@
 package com.actitime.generics;
 
+
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 import org.testng.Reporter;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
+
 public class Baseclass {
 	static {
 		System.setProperty("webdriver.chrome.driver","./driver/chromedriver.exe");
+		//System.setProperty("webdriver.gecko.driver","./driver/geckodriver.exe");
 	}
 	public WebDriver driver;
+ //@Parameters("browser")
 @BeforeClass
-public void openbrowser()
+public void openbrowser(String browser)
 {
 	Reporter.log("openbrowser",true);
+	//if(browser.equals("chrome"))
+	//{
 	 driver=new ChromeDriver();
 }
+// else if(browser.equals("firefox"))
+//	{
+		//driver=new FirefoxDriver();
+//	}
+	//driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	//driver.manage().window().maximize();
+//}
 @AfterClass
 public void closeBrowser()
 {
